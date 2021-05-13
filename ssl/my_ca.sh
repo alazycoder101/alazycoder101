e38872c2bd385f13fb099544829a4e52500d0554
lazycoder <alazycoder101@gmail.com>
2021-05-13 12:13:43 +0100

    script to sign certificate

--- ssl/my_ca.sh
+++ ssl/my_ca.sh
@@ -9,12 +9,9 @@ echo 1000 > serial
 
 echo "Type passphrase please [ENTER]:"
 read password
-echo $password > password
-# encrypt password
-openssl enc -aes-256-cbc -e -salt -v -in password -out password.enc
 
 # Generate Private Key
-openssl ecparam -genkey -name secp384r1 | openssl ec -aes256 -passout file:password.enc -out private/ca.key -passout file:password.enc
+openssl ecparam -genkey -name secp384r1 | openssl ec -aes256 -passout pass:$password -out private/ca.key
 
 # Create Certificate Authority Certificate
-openssl req -config openssl_root.cnf -new -x509 -sha384 -extensions v3_ca -key private/ca.key -out certs/ca.crt -passin file:password.enc
+openssl req -config openssl_root.cnf -new -x509 -sha384 -extensions v3_ca -key private/ca.key -out certs/ca.crt -passin pass:$password
