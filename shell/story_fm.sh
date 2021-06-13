198f4bd3d85b4e9aa4da1cffed4ed1f972386b85
lazycoder <alazycoder101@gmail.com>
2021-06-13 13:06:38 +0100

    download image and mp3 files

--- shell/story_fm.sh
+++ shell/story_fm.sh
@@ -1,14 +1,29 @@
-CNT=1
-if [ ! -f fm.xml ]; then
-  curl -L http://storyfm.cn/feed/episodes -o fm.xml
-fi
-files=$(xmllint --xpath "/rss/channel/item[position()>last()-$COUNT]/enclosure/@url" fm.xml)
-titles=$(xmllint --xpath "/rss/channel/item[position()>last()-$COUNT]/title/text()" fm.xml)
+RSS_URL=http://storyfm.cn/feed/episodes
+PUBLISHED_URL=https://storyfm.cn/published-stories/
+COUNT=1
 
-host="$(echo "cat /config/global/resources/default_setup/connection/host/text()" | xmllint --nocdata --shell file.xml | sed '1d;$d')"
-if [ ! -f index.html ]; then
-  curl https://storyfm.cn/published-stories/ -o index.html
+if [ ! -f fm.xml ]; then
+  curl -L $RSS_URL -o fm.xml
 fi
+
+
+files=$(xmllint --xpath "/rss/channel/item[position()>last()-$COUNT]/enclosure/@url" fm.xml)
+read -r -a mp3_files <<< "$files"
+
+if [ ! -f index.html ]; then
+  curl $PUBLISHED_URL -o index.html
+fi
+
 wechat=$(grep 'http://mp.weixin' -m $COUNT index.html)
-image_url=$(grep -Eo '<img class="rich_pages" [^>]+ data-s="[^>]+wx_fmt=jpeg"' -m 1 a.html|grep -Eo 'https:.*jpeg')
-curl $image_url -o $episode.jpg
+read -r -a titles <<< $(xmllint --xpath "/rss/channel/item[position()>last()-$COUNT]/title/text()" fm.xml)
+
+#[[ $url ~ (https://\\[^"\\]+) ]] && echo ${BASH_REMATCH[1]}
+for index in "${!mp3_files[@]}"
+do
+  eval "${mp3_files[index]}"
+  #curl -L -O -J $url
+  url=$(echo "${wechat[index]}"|grep -Eo 'http[^"]+')
+  image_url=$(curl -L $url|grep -Eo '<img class="rich_pages js_insertlocalimg" data-backh="804"[^>]+wx_fmt=jpeg"' -m 1|grep -Eo 'https:.*jpeg')
+  echo "image_url="${image_url}""
+  curl -L $image_url -o $index.jpg
+done
